0f0433d68dd814408b683c7990a6a23c4a96ff22
Alexander Berntsson <alexande.berntsson@gmail.com>
2022-12-13 07:17:31 +0100

    cleaned up d13

--- d13/main.py
+++ d13/main.py
@@ -15,32 +15,24 @@ def lt(a, b):
         return False
     i = 0
     for i in range(min(len(a), len(b))):
-        #print(a[i], b[i])
         if type(a[i]) == int:
             if type(b[i]) == int:
-                #print(f"int int {a[i]} {b[i]}")
                 if a[i] > b[i]:
                     return False
-                elif a[i] < b[i]:
-                     return True
+                elif a[i] < b[i]: 
+                    return True
             if type(b[i]) is list:
-                #print("int list")
                 res = lt([a[i]], b[i])
-                #print(f'res={res}')
-                if res is not None:
+                if res is not None: 
                     return res
         else:
             if type(b[i]) is list:
-                #print(f'list list {a[i]} {b[i]}')
                 res = lt(a[i], b[i])
-                #print(f'res={res}')
-                if res is not None:
+                if res is not None: 
                     return res      
             if type(b[i]) == int:
-                #print(f"list int {a[i]} {b[i]}")
                 res = lt(a[i], [b[i]])
-                #print(res)
-                if res is not None:
+                if res is not None: 
                     return res
 
     if len(a) > len(b):
@@ -50,34 +42,25 @@ def lt(a, b):
     
     return None
             
-
-lines  = [ eval(g) for group in groups for g in group.split('\n') ]
-#print(lines)
-lines.append([[2]])
-lines.append([[6]])
-lines = sorted(lines, key=cmp_to_key(lambda a,b : {True: -1, False: 1, None: 0}[lt(a,b)]))
-#lines.sort(key=lambda l: len(l))
-for line in lines:
-    print(line)
-print(lines.index([[2]])+1)
-print(lines.index([[6]])+1)
-print((lines.index([[2]])+1)*(lines.index([[6]])+1))
+def part2():
+    lines  = [ eval(g) for group in groups for g in group.split('\n') ]
+    lines.append([[2]])
+    lines.append([[6]])
+    lines = sorted(lines, key=cmp_to_key(lambda a,b : {True: -1, False: 1, None: 0}[lt(a,b)]))
+    print((lines.index([[2]])+1)*(lines.index([[6]])+1))
 
 
-        
-# cnt = 0
-# for i, group in enumerate(groups):
-#     left, right = group.split('\n')
-#     #left = left.replace('[', '(')
-#     #left = left.replace(']', ')')
-#     #print(left)
-#     left = eval(left)
-#     #right = right.replace('[', '(')
-#     #right = right.replace(']', ')')
-#     #print(right)
-#     right = eval(right)
+def part1():
+    cnt = 0
+    for i, group in enumerate(groups):
+        left, right = group.split('\n')
+        left = eval(left)
+        right = eval(right)
 
-#     #res = lt(left, right)
-#     if left < right:
-#         cnt += 1+i
-# print(cnt)
+        cm = lt(left, right)
+        if cm:
+            cnt += 1+i
+    print(cnt)
+
+part1()    
+part2()
